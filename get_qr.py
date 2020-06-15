5e26e1b26c5a45989b0b1fd6cc2f52c702ef359a
ongyuann <32030081+ongyuann@users.noreply.github.com>
2020-06-15 15:44:28 +0800

    Update get_qr.py

--- get_qr.py
+++ get_qr.py
@@ -25,7 +25,7 @@ auth_header = {'Authorization':ur_api_token}
 local_url = http_or_https + '://127.0.0.1:3333/api/campaigns/' + campaign_id + "/results"
 
 try:
-    r = requests.get(local_url,headers=auth_header)
+    r = requests.get(local_url,headers=auth_header,verify=False)
 except:
     print ("[!] something went wrong - we can't connect to your gophish API. check if you can connect to API manually and troubleshoot:\n[+] curl localhost:3333/api/campaigns/" + campaign_id + "/results -H 'Authorization: " + ur_api_token + "' | grep 'id' | grep -v 'campaign' | cut -d':' -f2 | cut -d'\"' -f2")
     sys.exit()
